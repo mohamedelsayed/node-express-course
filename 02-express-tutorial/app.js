5b0608bace8c0f8edb6711e0297a701b9d12939b
Mohamed Elsayed <m.elsayed@advantive.net>
2022-02-21 03:41:09 +0200

    express params

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,53 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-app.use(express.static('./public'))
+const {
+    products
+} = require('./data')
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+    res.send('<h1>Home Page</h1><a href="/api/products/">products</a>');
+})
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {
+            id,
+            name,
+            image
+        } = product;
+        return {
+            id,
+            name,
+            image
+        };
+    })
+    res.json(newProducts);
 });
-app.all('*', (req, res) => {
-    res.status(404).send('resource not found');
+app.get('/api/products/:productId', (req, res) => {
+    console.log(req.params);
+    const {
+        productId
+    } = req.params;
+    const singleProduct = products.find((product) => product.id === Number(productId));
+    if (!singleProduct) {
+        return res.status(404).send('product not found');
+    }
+    return res.json(singleProduct);
 });
+
+app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
+    console.log(req.params);
+    res.send('Hello world');
+});
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query);
+    const {
+        search,
+        limit
+    } = req.query;
+    let sortedProducts = [...products];
+    res.send('Hello world');
+});
+
 app.listen(5000, () => {
-    console.log('server is listening on port 5000...');
-});
+    console.log('Server is listening on port 5000...');
+})
