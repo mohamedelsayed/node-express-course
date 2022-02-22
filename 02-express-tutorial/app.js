dab1b19616c23ae48dfe7ad77c6f06a57d55373d
Mohamed Elsayed <m.elsayed@advantive.net>
2022-02-22 02:56:06 +0200

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -45,7 +45,22 @@ app.get('/api/v1/query', (req, res) => {
         limit
     } = req.query;
     let sortedProducts = [...products];
-    res.send('Hello world');
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search);
+        });
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if (sortedProducts.length < 1) {
+        // res.status(200).send('np products matched your search');
+        return res.status(200).json({
+            success: true,
+            data: []
+        });
+    }
+    res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
