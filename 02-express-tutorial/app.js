bda1c5520a6d90998c1f8e9aa457d45eb38abd50
Mohamed Elsayed <m.elsayed@advantive.net>
2022-02-22 03:47:37 +0200

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,68 +1,20 @@
 const express = require('express');
 const app = express();
-const {
-    products
-} = require('./data')
+const logger = require('./logger');
+const authorize = require('./authorize');
+app.use([logger, authorize]);
 app.get('/', (req, res) => {
-    res.send('<h1>Home Page</h1><a href="/api/products/">products</a>');
-})
+    res.send('Home');
+});
+app.get('/about', (req, res) => {
+    res.send('About');
+});
 app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const {
-            id,
-            name,
-            image
-        } = product;
-        return {
-            id,
-            name,
-            image
-        };
-    })
-    res.json(newProducts);
+    res.send('Products');
 });
-app.get('/api/products/:productId', (req, res) => {
-    console.log(req.params);
-    const {
-        productId
-    } = req.params;
-    const singleProduct = products.find((product) => product.id === Number(productId));
-    if (!singleProduct) {
-        return res.status(404).send('product not found');
-    }
-    return res.json(singleProduct);
+app.get('/api/items', (req, res) => {
+    res.send('Items');
 });
-
-app.get('/api/products/:productId/reviews/:reviewId', (req, res) => {
-    console.log(req.params);
-    res.send('Hello world');
-});
-
-app.get('/api/v1/query', (req, res) => {
-    console.log(req.query);
-    const {
-        search,
-        limit
-    } = req.query;
-    let sortedProducts = [...products];
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search);
-        });
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit));
-    }
-    if (sortedProducts.length < 1) {
-        // res.status(200).send('np products matched your search');
-        return res.status(200).json({
-            success: true,
-            data: []
-        });
-    }
-    res.status(200).json(sortedProducts);
-});
-
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000...');
+    console.log('Server is listening on port 5000....');
 })
