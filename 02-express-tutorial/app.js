417e08c379b5af157c5093bedd4f6a34b304ddf8
Mohamed Elsayed <m.elsayed@advantive.net>
2022-02-23 05:51:56 +0200

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const app = express();
-const logger = require('./logger');
-const authorize = require('./authorize');
-app.use([logger, authorize]);
-app.get('/', (req, res) => {
-    res.send('Home');
+let {
+    people
+} = require('./data');
+//parse form data
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use(express.static('./methods-public'));
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: people
+    });
 });
-app.get('/about', (req, res) => {
-    res.send('About');
+
+app.post('/login', (req, res) => {
+    // console.log(req.body);
+    const {
+        name
+    } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`);
+    }
+    res.status(401).send('Please Provide Credentials');
 });
-app.get('/api/products', (req, res) => {
-    res.send('Products');
-});
-app.get('/api/items', (req, res) => {
-    res.send('Items');
-});
+
 app.listen(5000, () => {
     console.log('Server is listening on port 5000....');
 })
