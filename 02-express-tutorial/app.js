3d440339c07e3662cd01731c8c2754c681893501
Mohamed Elsayed <m.elsayed@advantive.net>
2022-02-24 05:48:16 +0200

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,6 +7,7 @@ let {
 app.use(express.urlencoded({
     extended: false
 }));
+app.use(express.json());
 
 app.use(express.static('./methods-public'));
 
@@ -14,6 +15,66 @@ app.get('/api/people', (req, res) => {
     res.status(200).json({
         success: true,
         data: people
+    });
+});
+
+app.post('/api/people', (req, res) => {
+    const {
+        name
+    } = req.body;
+    if (!name) {
+        return res.status(400).json({
+            status: false,
+            msg: 'please provide name value'
+        });
+    }
+    res.status(201).json({
+        success: true,
+        person: name
+    });
+});
+
+app.put('/api/people/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const {
+        name
+    } = req.body;
+    const person = people.find((person) => person.id === Number(id));
+    if (!person) {
+        return res.status(400).json({
+            status: false,
+            msg: `no person with id ${id}`
+        });
+    }
+    const newPeople = people.map((person) => {
+        if (person.id == Number(id)) {
+            person.name = name;
+        }
+        return person
+    });
+    res.status(200).json({
+        success: true,
+        data: newPeople
+    });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+    const {
+        id
+    } = req.params;
+    const person = people.find((person) => person.id === Number(id));
+    if (!person) {
+        return res.status(400).json({
+            status: false,
+            msg: `no person with id ${id}`
+        });
+    }
+    const newPeople = people.filter((person) => person.id !== Number(id));
+    res.status(200).json({
+        success: true,
+        data: newPeople
     });
 });
 
